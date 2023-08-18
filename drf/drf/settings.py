8bdf130972da0e57dcb153095411c265eb166b88
shahidullah <shahidullah.piyaro@gmail.com>
2023-08-18 03:01:15 -0700

    filters, search filter and pagination using DRF

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'user_app',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- drf/first_app/urls.py
+++ drf/first_app/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .views import ProductViewSet, ProductReviewViewSet
 
 router = routers.DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('reviews', ProductReviewViewSet)
+router.register('products', ProductViewSet, basename='product')
+router.register('reviews', ProductReviewViewSet, basename='product-review')
 
 urlpatterns = [
     path('', include(router.urls)),
